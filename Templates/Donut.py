832947c27f6807991a573dcc213645a8324f81f7
Sean Goossens <sean.goossens@gmail.com>
2017-08-08 15:46:02 -0600

    Added some templates

---
+++ Templates/Donut.py
@@ -0,0 +1,84 @@
+{
+    "chart": {
+        "width": 239.771,
+        "height": 133.28,
+        "type": "pie",
+        "style": {
+            "fontFamily": "Arial",
+            "textAlign": "center",
+            "cursor": true,
+            "dataLabels": {
+                "enabled": true
+            }
+        },
+        "renderTo": "highchartContainer"
+    },
+    "colors": [
+        "#0B325F",
+        "#0A5EB2",
+        "#00A2E0",
+        "#008B94",
+        "#56D8C2",
+        "#00B7BD",
+        "#B5B5B5",
+        "#636363"
+    ],
+    "plotOptions": {
+        "pie": {
+            "allowPointSelect": true,
+            "cursor": true,
+            "innerSize": 78.132,
+            "size": 125.413,
+            "showInLegend": true,
+            "dataLabels": {
+                "enabled": false
+            },
+            "borderWidth": 0,
+            "center": [
+                null,
+                null
+            ]
+        },
+        "series": {}
+    },
+    "legend": {
+        "itemWidth": 100,
+        "align": "center",
+        "enabled": false,
+        "layout": "horizontal",
+        "symbolRadius": 0,
+        "padding": 0,
+        "itemStyle": {
+            "fontSize": "8px",
+            "fontWeight": "normal"
+        }
+    },
+    "title": {
+        "text": null
+    },
+    "yAxis": {
+        "title": {
+            "text": null,
+            "style": {
+                "fontSize": "8px"
+            }
+        },
+        "labels": {
+            "format": "{Series 1}"
+        }
+    },
+    "xAxis": {
+        "title": {
+            "style": {
+                "fontSize": "9px"
+            }
+        },
+        "type": "category"
+    },
+    "credits": {
+        "enabled": false
+    },
+    "exporting": {
+        "scale": 1
+    }
+}
---
+++ Templates/Legend.py
@@ -0,0 +1,69 @@
+{
+    "chart": {
+        "width":239.271,
+        "height": 63.654,
+        "type": "pie",
+        "spacing": 0,
+        "padding": 0,
+        "style": {
+            "fontFamily": "Arial",
+            "textAlign": "center",
+            "cursor": true,
+            "dataLabels": {
+                "enabled": false
+            }
+        },
+        "renderTo": "highchartContainer"
+    },
+    "colors": [
+        "#0B325F",
+        "#0A5EB2",
+        "#00A2E0",
+        "#008B94",
+        "#56D8C2",
+        "#00B7BD",
+        "#B5B5B5",
+        "#636363"
+    ],
+    "plotOptions": {
+        "pie": {
+            "allowPointSelect": true,
+            "cursor": true,
+            "innerSize": "100%",
+            "size": "0px",
+            "showInLegend": true,
+            "dataLabels": {
+                "enabled": false
+            },
+            "borderWidth": 0,
+            "center": [
+                null,
+                null
+            ]
+        },
+        "series": {}
+    },
+    "legend": {
+        "itemWidth": 120,
+        "width": 251.983,
+        "align": "center",
+        "layout": "horizontal",
+        "verticalAlign": "top",
+        "symbolRadius": 0,
+        "symbolPadding": 0,
+        "floating": true,
+        "itemStyle": {
+            "fontSize": "8px",
+            "fontWeight": "normal"
+        }
+    },
+    "title": {
+        "text": null
+    },
+    "credits": {
+        "enabled": false
+    },
+    "exporting": {
+        "scale": 1
+    }
+}
