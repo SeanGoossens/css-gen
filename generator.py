4375fcee9e32b399dbb747dfe457260d6d223195
Sean Goossens <sean.goossens@gmail.com>
2017-08-25 16:01:46 -0600

    Reformat

--- generator.py
+++ generator.py
@@ -50,317 +50,35 @@ def build():
     template = input('Which template? Type clearwater or path: ')
 
     if template == 'clearwater':
-        chart = input('Chart type: ')
-        if chart == 'column':
-            width = input('Width: ')
-            height = input('Height: ')
-            dataLabels = input('Data labels enabled? Type true or false: ')
-            legendEnabled = input('Legend enabled? Type true or false: ')
-            print("""{
-        "colors": [
-            """ + clearwaterColors + """], 
-        
-        "chart": {
-            "type": """ +
-                  '"' + chart + '",' + """
-            "width": """ + width + ',' + """
-            "height": """ + height + ',' + """
-            "style": {
-                "fontfamily": """ + clearwaterFont + """
-                },
-            "spacingTop": 0,
-            "spacingRight": 0,
-            "spacingLeft": 0,
-            "spacingBottom": 0,
-            "renderTo": "highchartContainer"
-        },
-        
-        "plotOptions": {
-            "series": {
-                "dataLabels": {
-                    "enabled": """ + dataLabels + """,
-                    "format": "{y}",
-                    "padding": 2,
-                    "style": {
-                        "fontSize": "8px",
-                        "letterSpacing": ".05px",
-                        "textShadow": "false"
-                    },
-                    "color": """ + clearwaterChartColor + """,
-                    "y": -2
-                }
-            },
-            "column": {
-                "borderWidth": 0,
-                "groupPadding": 0.06,
-                "pointPadding": 0,
-                "pointRange": 0
-            }
-        },
-        "title": {
-            "text": null
-        },
-        "yAxis": {
-            "title": {
-                "text": null
-            },
-            "lineWidth": 0.5,
-            "lineColor": """ + clearwaterLineColor + """,
-            "tickColor": "#a7a9ac",
-            "tickPosition": "inside",
-            "tickPositions": "",
-            "tickWidth": 0.5,
-            "tickLength": 4,
-            "gridLineWidth": 0,
-            "gridLineInterpolation": null,
-            "labels": {
-                "enabled": true,
-                "x": -5,
-                "format": "{value}%",
-                "style": {
-                    "fontSize": "8px",
-                    "color": """ + clearwaterChartColor + """
-                }
-            },
-            "type": "value"
-        },
-        "legend": {
-            "enabled": """ + legendEnabled + """,
-            "align": "right",
-            "verticalAlign": "top",
-            "itemStyle": {
-                "fontWeight": "normal",
-                "fontSize": "8px",
-                "color": """ + clearwaterChartColor + """
-            },
-            "symbolPadding": 3,
-            "symbolRadius": 0,
-            "symbolWidth": 7,
-            "symbolHeight": 7
-        },
-        "credits": {
-            "enabled": false
-        },
-        "xAxis": {
-            "lineColor": """ + clearwaterLineColor + """,
-            "tickColor": "#a7a9ac",
-            "tickPosition": "inside",
-            "tickLength": 4,
-            "type": "category",
-            "tickWidth": 0.5,
-            "lineWidth": 0.5,
-            "labels": {
-                "enabled": true,
-                "style": {
-                    "fontSize": "8px",
-                    "color": """ + clearwaterChartColor + """
-                }
-            }
-        },
-        "exporting": {
-            "scale": 1
-        }
-    }""")
-        elif chart == "area":
-            width = input('Width: ')
-            height = input('Height: ')
-            dataLabels = input('Data labels enabled? Type true or false: ')
-            legendEnabled = input('Legend enabled? Type true or false: ')
-            print("""{
-                    "colors": [
-                        """ + clearwaterColors + """], 
+        colors = clearwaterColors
+        font = clearwaterFont
+        chartColor = clearwaterChartColor
+        lineColor = clearwaterLineColor
 
-                    "chart": {
-                        "type": """ +
-                  '"' + chart + '",' + """
-                        "width": """ + width + ',' + """
-                        "height": """ + height + ',' + """
-                        "style": {
-                            "fontfamily": """ + clearwaterFont + """
-                            },
-                        "spacingTop": 0,
-                        "spacingRight": 0,
-                        "spacingLeft": 0,
-                        "spacingBottom": 0,
-                        "renderTo": "highchartContainer"
-                    },
+    elif template == 'path':
+        colors = pathColors
+        font = pathFont
+        chartColor = pathChartColor
+        lineColor = pathLineColor
 
-                    "plotOptions": {
-                        "series": {
-                            "dataLabels": {
-                                "enabled": """ + dataLabels + """,
-                                "format": "{y}",
-                                "padding": 2,
-                                "style": {
-                                    "fontSize": "8px",
-                                    "letterSpacing": ".05px",
-                                    "textShadow": "false"
-                                },
-                                "color": """ + clearwaterChartColor + """,
-                                "y": -2
-                            }
-                        },
-                    },
-                    "title": {
-                        "text": null
-                    },
-                    "yAxis": {
-                        "title": {
-                            "text": null
-                        },
-                        "lineWidth": 0.5,
-                        "lineColor": """ + clearwaterLineColor + """,
-                        "tickColor": "#a7a9ac",
-                        "tickPosition": "inside",
-                        "tickPositions": "",
-                        "tickWidth": 0.5,
-                        "tickLength": 4,
-                        "gridLineWidth": 0,
-                        "gridLineInterpolation": null,
-                        "labels": {
-                            "enabled": true,
-                            "x": -5,
-                            "format": "{value}%",
-                            "style": {
-                                "fontSize": "8px",
-                                "color": """ + clearwaterChartColor + """
-                            }
-                        },
-                        "type": "value"
-                    },
-                    "legend": {
-                        "enabled": """ + legendEnabled + """,
-                        "align": "right",
-                        "verticalAlign": "top",
-                        "itemStyle": {
-                            "fontWeight": "normal",
-                            "fontSize": "8px",
-                            "color": """ + clearwaterChartColor + """
-                        },
-                        "symbolPadding": 3,
-                        "symbolRadius": 0,
-                        "symbolWidth": 7,
-                        "symbolHeight": 7
-                    },
-                    "credits": {
-                        "enabled": false
-                    },
-                    "xAxis": {
-                        "lineColor": """ + clearwaterLineColor + """,
-                        "tickColor": "#a7a9ac",
-                        "tickPosition": "outside",
-                        "tickLength": 4,
-                        "type": "category",
-                        "tickWidth": 0.5,
-                        "lineWidth": 0.5,
-                        "labels": {
-                            "enabled": true,
-                            "style": {
-                                "fontSize": "8px",
-                                "color": """ + clearwaterChartColor + """
-                            }
-                        }
-                    },
-                    "exporting": {
-                        "scale": 1
-                    }
-                }""")
-        elif chart == "pie":
-            width = input('Width: ')
-            height = input('Height: ')
-            print("""
-{
-    "colors": [
-        """ + clearwaterColors + """
-    ], 
-    "chart": {
-        "type": """ + '"' + chart + '",' + """
-        "width": """ + width + ',' + """
-        "height": """ + height + ',' + """
-        "style": {
-            "fontfamily": """ + clearwaterFont + """
-            },
-        "renderTo": "highchartContainer"
-        },
+    chart = input('Chart type: ')
 
-     "plotOptions": {
-        "pie": {
-            "allowPointSelect": true,
-            "cursor": true,
-            "innerSize": "60%",
-            "size": 135,
-            "showInLegend": true,
-            "dataLabels": {
-                "enabled": false
-            },
-            "borderWidth": 0,
-            "center": [
-                null,
-                null
-            ]
-        },
-        "series": {}
-    },
-    "legend": {
-        "itemWidth": 105,
-        "align": "left",
-        "enabled": false,
-        "layout": "horizontal",
-        "symbolRadius": 0,
-        "y": -1,
-        "floating": false,
-        "itemStyle": {
-            "fontSize": "9px",
-            "width": 120,
-            "fontWeight": "normal"
-        }
-    },
-    "title": {
-        "text": null
-    },
-    "yAxis": {
-        "title": {
-            "text": null,
-            "style": {
-                "fontSize": "8px"
-            }
-        },
-        "labels": {
-            "format": "{Series 1}"
-        }
-    },
-    "xAxis": {
-        "title": {
-            "style": {
-                "fontSize": "8px"
-            }
-        },
-        "type": "category"
-    },
-    "credits": {
-        "enabled": false
-    },
-    "exporting": {
-        "scale": 1
-    }
-}""")
-    elif (template == 'path'):
-        chart = input('Chart type: ')
+    if chart == 'column':
         width = input('Width: ')
         height = input('Height: ')
         dataLabels = input('Data labels enabled? Type true or false: ')
         legendEnabled = input('Legend enabled? Type true or false: ')
         print("""{
     "colors": [
-        """ + clearwaterColors + """], 
-
+        """ + colors + """], 
+    
     "chart": {
         "type": """ +
               '"' + chart + '",' + """
         "width": """ + width + ',' + """
         "height": """ + height + ',' + """
         "style": {
-            "fontfamily": """ + pathFont + """
+            "fontfamily": """ + font + """
             },
         "spacingTop": 0,
         "spacingRight": 0,
@@ -368,7 +86,7 @@ def build():
         "spacingBottom": 0,
         "renderTo": "highchartContainer"
     },
-
+    
     "plotOptions": {
         "series": {
             "dataLabels": {
@@ -380,7 +98,7 @@ def build():
                     "letterSpacing": ".05px",
                     "textShadow": "false"
                 },
-                "color": """ + pathChartColor + """,
+                "color": """ + chartColor + """,
                 "y": -2
             }
         },
@@ -399,7 +117,7 @@ def build():
             "text": null
         },
         "lineWidth": 0.5,
-        "lineColor": """ + pathLineColor + """,
+        "lineColor": """ + lineColor + """,
         "tickColor": "#a7a9ac",
         "tickPosition": "inside",
         "tickPositions": "",
@@ -413,7 +131,7 @@ def build():
             "format": "{value}%",
             "style": {
                 "fontSize": "8px",
-                "color": """ + pathChartColor + """
+                "color": """ + chartColor + """
             }
         },
         "type": "value"
@@ -425,7 +143,7 @@ def build():
         "itemStyle": {
             "fontWeight": "normal",
             "fontSize": "8px",
-            "color": """ + pathChartColor + """
+            "color": """ + chartColor + """
         },
         "symbolPadding": 3,
         "symbolRadius": 0,
@@ -436,7 +154,7 @@ def build():
         "enabled": false
     },
     "xAxis": {
-        "lineColor": """ + pathLineColor + """,
+        "lineColor": """ + lineColor + """,
         "tickColor": "#a7a9ac",
         "tickPosition": "inside",
         "tickLength": 4,
@@ -447,14 +165,202 @@ def build():
             "enabled": true,
             "style": {
                 "fontSize": "8px",
-                "color": """ + pathChartColor + """
+                "color": """ + chartColor + """
             }
         }
     },
     "exporting": {
         "scale": 1
     }
-    }""")
+}""")
+    elif chart == "area":
+        width = input('Width: ')
+        height = input('Height: ')
+        dataLabels = input('Data labels enabled? Type true or false: ')
+        legendEnabled = input('Legend enabled? Type true or false: ')
+        print("""{
+                "colors": [
+                    """ + colors + """], 
 
+                "chart": {
+                    "type": """ +
+              '"' + chart + '",' + """
+                    "width": """ + width + ',' + """
+                    "height": """ + height + ',' + """
+                    "style": {
+                        "fontfamily": """ + font + """
+                        },
+                    "spacingTop": 0,
+                    "spacingRight": 0,
+                    "spacingLeft": 0,
+                    "spacingBottom": 0,
+                    "renderTo": "highchartContainer"
+                },
+
+                "plotOptions": {
+                    "series": {
+                        "dataLabels": {
+                            "enabled": """ + dataLabels + """,
+                            "format": "{y}",
+                            "padding": 2,
+                            "style": {
+                                "fontSize": "8px",
+                                "letterSpacing": ".05px",
+                                "textShadow": "false"
+                            },
+                            "color": """ + chartColor + """,
+                            "y": -2
+                        }
+                    },
+                },
+                "title": {
+                    "text": null
+                },
+                "yAxis": {
+                    "title": {
+                        "text": null
+                    },
+                    "lineWidth": 0.5,
+                    "lineColor": """ + lineColor + """,
+                    "tickColor": "#a7a9ac",
+                    "tickPosition": "inside",
+                    "tickPositions": "",
+                    "tickWidth": 0.5,
+                    "tickLength": 4,
+                    "gridLineWidth": 0,
+                    "gridLineInterpolation": null,
+                    "labels": {
+                        "enabled": true,
+                        "x": -5,
+                        "format": "{value}%",
+                        "style": {
+                            "fontSize": "8px",
+                            "color": """ + chartColor + """
+                        }
+                    },
+                    "type": "value"
+                },
+                "legend": {
+                    "enabled": """ + legendEnabled + """,
+                    "align": "right",
+                    "verticalAlign": "top",
+                    "itemStyle": {
+                        "fontWeight": "normal",
+                        "fontSize": "8px",
+                        "color": """ + chartColor + """
+                    },
+                    "symbolPadding": 3,
+                    "symbolRadius": 0,
+                    "symbolWidth": 7,
+                    "symbolHeight": 7
+                },
+                "credits": {
+                    "enabled": false
+                },
+                "xAxis": {
+                    "lineColor": """ + lineColor + """,
+                    "tickColor": "#a7a9ac",
+                    "tickPosition": "outside",
+                    "tickLength": 4,
+                    "type": "category",
+                    "tickWidth": 0.5,
+                    "lineWidth": 0.5,
+                    "labels": {
+                        "enabled": true,
+                        "style": {
+                            "fontSize": "8px",
+                            "color": """ + chartColor + """
+                        }
+                    }
+                },
+                "exporting": {
+                    "scale": 1
+                }
+            }""")
+    elif chart == "pie":
+        width = input('Width: ')
+        height = input('Height: ')
+        print("""
+{
+"colors": ["""
+    + colors + """
+], 
+"chart": {
+    "type": """ + '"' + chart + '",' + """
+    "width": """ + width + ',' + """
+    "height": """ + height + ',' + """
+    "style": {
+        "fontfamily": """ + font + """
+        },
+    "renderTo": "highchartContainer"
+    },
+
+ "plotOptions": {
+    "pie": {
+        "allowPointSelect": true,
+        "cursor": true,
+        "innerSize": "60%",
+        "size": 135,
+        "showInLegend": true,
+        "dataLabels": {
+            "enabled": false
+        },
+        "borderWidth": 0,
+        "center": [
+            null,
+            null
+        ]
+    },
+    "series": {}
+},
+
+"legend": {
+    "itemWidth": 105,
+    "align": "left",
+    "enabled": false,
+    "layout": "horizontal",
+    "symbolRadius": 0,
+    "y": -1,
+    "floating": false,
+    "itemStyle": {
+        "fontSize": "9px",
+        "width": 120,
+        "fontWeight": "normal"
+    }
+},
+
+"title": {
+    "text": null
+},
+
+"yAxis": {
+    "title": {
+        "text": null,
+        "style": {
+            "fontSize": "8px"
+        }
+    },
+    "labels": {
+        "format": "{Series 1}"
+    }
+},
+
+"xAxis": {
+    "title": {
+        "style": {
+            "fontSize": "8px"
+        }
+    },
+    "type": "category"
+},
+
+"credits": {
+    "enabled": false
+},
+
+"exporting": {
+    "scale": 1
+}
+}""")
 
 build();
